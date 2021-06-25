585efd6443c7be45150041ed1bd0d02b5782d8fe
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-06-25 18:42:57 +0300

    добавила решение задач автопроверки

--- js/script.js
+++ js/script.js
@@ -1586,3 +1586,226 @@
 //  37
 
 //  теория (Сортировка объектов)
+// const students = [
+//   { name: 'Манго', score: 83 },
+//   { name: 'Поли', score: 59 },
+//   { name: 'Аякс', score: 37 },
+//   { name: 'Киви', score: 94 },
+// ];
+
+// const inAscendingScoreOrder = [...students].sort(
+//   (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+// );
+
+// const inDescendingScoreOrder = [...students].sort(
+//   (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+// );
+
+// const inAlphabeticalOrder = [...students].sort((firstStudent, secondStudent) =>
+//   firstStudent.name.localeCompare(secondStudent.name)
+// );
+// console.log(inAscendingScoreOrder);
+// console.log(inDescendingScoreOrder);
+// console.log(inAlphabeticalOrder);
+
+//  задача
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//   { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
+// // Пиши код ниже этой строки
+
+// const sortedByAuthorName = [...books].sort((firstAuthor, secondAuthor) => firstAuthor.author.localeCompare(secondAuthor.author));
+
+// const sortedByReversedAuthorName = [...books].sort((firstAuthor, secondAuthor) => secondAuthor.author.localeCompare(firstAuthor.author));
+
+// const sortedByAscendingRating = [...books].sort((firstRating, secondRating) => firstRating.rating - secondRating.rating);
+
+// const sortedByDescentingRating = [...books].sort((firstRating, secondRating) => secondRating.rating - firstRating.rating);
+
+// console.log(sortedByAuthorName);
+// console.log(sortedByReversedAuthorName);
+// console.log(sortedByAscendingRating);
+// console.log(sortedByDescentingRating);
+
+
+//  38
+// Задача. Сортировка по балансу
+
+// Пиши код ниже этой строки
+// const sortByAscendingBalance = users => {
+//    return [...users].sort((firstBalance, secondBalance) => firstBalance.balance - secondBalance.balance)
+// };
+// // Пиши код выше этой строки
+
+
+//  39
+//  Задача. Сортировка по количеству друзей
+
+// // Пиши код ниже этой строки
+// const sortByDescendingFriendCount = users => {
+//    return [...users].sort((firstAmountOfFriends, secondAmountOfFriends) => secondAmountOfFriends.friends.length - firstAmountOfFriends.friends.length)
+// };
+// // Пиши код выше этой строки
+
+
+//  40
+// Задача. Сортировка по имени
+
+// // Пиши код ниже этой строки
+// const sortByName = users => {
+//    return [...users].sort((firstUser, secondUser) => firstUser.name.localeCompare(secondUser.name))
+// };
+// // Пиши код выше этой строки
+
+
+//  41
+//  теория (Цепочки методов (чейнинг, chaining))
+
+// const students = [
+//   { name: 'Манго', score: 83, courses: ['математика', 'физика'] },
+//   { name: 'Поли', score: 59, courses: ['информатика', 'математика'] },
+//   { name: 'Аякс', score: 37, courses: ['физика', 'биология'] },
+//   { name: 'Киви', score: 94, courses: ['литература', 'информатика'] },
+// ];
+// const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
+// const names = sortedByAscendingScore.map((student) => student.name);
+
+// console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
+
+// const names = [...students]
+//   .sort((a, b) => a.score - b.score)
+//   .map((student) => student.name);
+
+// console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
+// const uniqueSortedCourses = students
+//   .flatMap((student) => student.courses)
+//   .filter((course, index, array) => array.indexOf(course) === index)
+//   .sort((a, b) => a.localeCompare(b));
+
+// console.log(uniqueSortedCourses); // ['биология', 'информатика', 'литература', 'математика', 'физика']
+
+// Получим сортированный по алфавиту массив уникальных посещаемых предметов.
+// const uniqueSortedCourses = students
+//   .flatMap((student) => student.courses)
+//   .filter((course, index, array) => array.indexOf(course) === index)
+//   .sort((a, b) => a.localeCompare(b));
+
+// console.log(uniqueSortedCourses); // ['биология', 'информатика', 'литература', 'математика', 'физика']
+
+//  задача
+// const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 8.14 },
+//   { title: 'Сны В Ведьмином Доме', author: 'Говард Лавкрафт', rating: 8.67 }
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Пиши код ниже этой строки
+
+// const names = books.filter(book => book.rating > MIN_BOOK_RATING).map(book => book.author).sort((a, b) => a.localeCompare(b));
+
+// console.log(names); // ["Бернард Корнуэлл", "Говард Лавкрафт", "Ли Танит", "Роберт Шекли"]
+
+
+//  42
+// Задача. Пользователи и друзья
+
+// // Пиши код ниже этой строки
+// const getNamesSortedByFriendCount = users => {
+//    return [...users].sort((firstAmountOfFriends, secondAmountOfFriends) => 
+//  firstAmountOfFriends.friends.length - secondAmountOfFriends.friends.length).map(user => user.name)
+// };
+// // Пиши код выше этой строки
+
+
+//  43
+// Задача. Имена друзей
+
+// // Пиши код ниже этой строки
+// const getSortedFriends = users => {
+//    return users.flatMap(user => user.friends).filter((friend, index, array) => array.indexOf(friend) === index).sort((a,b) => a.localeCompare(b))
+// };
+// // Пиши код выше этой строк
+
+
+//  44
+// Задача. Общий баланс
+
+// const users =
+// [
+//   {
+//     name: 'Moore Hensley',
+//     email: 'moorehensley@indexia.com',
+//     eyeColor: 'blue',
+//     friends: ['Sharron Pace'],
+//     isActive: false,
+//     balance: 2811,
+//     gender: 'male'
+//   },
+//   {
+//     name: 'Sharlene Bush',
+//     email: 'sharlenebush@tubesys.com',
+//     eyeColor: 'blue',
+//     friends: ['Briana Decker', 'Sharron Pace'],
+//     isActive: true,
+//     balance: 3821,
+//     gender: 'female'
+//   },
+//   {
+//     name: 'Ross Vazquez',
+//     email: 'rossvazquez@xinware.com',
+//     eyeColor: 'green',
+//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+//     isActive: false,
+//     balance: 3793,
+//     gender: 'male'
+//   },
+//   {
+//     name: 'Elma Head',
+//     email: 'elmahead@omatom.com',
+//     eyeColor: 'green',
+//     friends: ['Goldie Gentry', 'Aisha Tran'],
+//     isActive: true,
+//     balance: 2278,
+//     gender: 'female'
+//   },
+//   {
+//     name: 'Carey Barr',
+//     email: 'careybarr@nurali.com',
+//     eyeColor: 'blue',
+//     friends: ['Jordan Sampson', 'Eddie Strong', 'Adrian Cross'],
+//     isActive: true,
+//     balance: 3951,
+//     gender: 'male'
+//   },
+//   {
+//     name: 'Blackburn Dotson',
+//     email: 'blackburndotson@furnigeer.com',
+//     eyeColor: 'brown',
+//     friends: ['Jacklyn Lucas', 'Linda Chapman', 'Adrian Cross', 'Solomon Fokes'],
+//     isActive: false,
+//     balance: 1498,
+//     gender: 'male'
+//   },
+//   {
+//     name: 'Sheree Anthony',
+//     email: 'shereeanthony@kog.com',
+//     eyeColor: 'brown',
+//     friends: ['Goldie Gentry', 'Briana Decker'],
+//     isActive: true,
+//     balance: 2764,
+//     gender: 'female'
+//   }
+// ]
+// // Пиши код ниже этой строки
+// const getTotalBalanceByGender = (users, gender) => {
+//    return users.filter(user => user.gender === gender).reduce((total, user) => total + user.balance, 0)
+// };
+// // Пиши код выше этой строки
+// console.log(getTotalBalanceByGender(users, 'male'));
+// console.log(getTotalBalanceByGender(users, 'female'));
